cc707ca2f9534228489f4aab701147512108b10d
Sirma-cloud <kevinkiplimo99@gmail.com>
2024-02-21 21:27:47 +0300

    Added the task mapper

---
+++ src/main/java/com/example/taskManager/mapper/TaskMapper.java
@@ -0,0 +1,23 @@
+package com.example.taskManager.mapper;
+
+import com.example.taskManager.dto.TaskDTO;
+import com.example.taskManager.model.Task;
+
+public class TaskMapper {
+    public static TaskDTO MapToDto(Task task) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setName(task.getName());
+        taskDTO.setDescription(task.getDescription());
+        taskDTO.setStatus(task.getStatus());
+
+        return taskDTO;
+    }
+    public static Task mapToTask(TaskDTO dto) {
+        Task task = new Task();
+        task.setName(dto.getName());
+        task.setDescription(dto.getDescription());
+        task.setStatus(dto.getStatus());
+        return task;
+    }
+
+}
